7c0efef266c97809b05fe7efa4cbbfd72c1a0f45
kks <kks@example.com>
2024-12-31 16:46:17 +0900

    APP VUE

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,7 @@
-<template>
-  <div id="app">
-    <HealthMap />
-  </div>
-  <div>
-    <naver-map style="width: 100%; height: 400px" :map-options="mapOptions">
-        <naver-marker latitude="37.51347" longitude="127.041722" />
-    </naver-map>
-  </div>
-</template>
-
 <script setup>
 import HealthMap from "./components/sch/area-sch.vue";
+import Header from "./components/main/main-header.vue";
+import Footer from "./components/main/main-footer.vue";
 import { NaverMap } from 'vue3-naver-maps'
 import { NaverMarker } from 'vue3-naver-maps'
 const mapOptions = {
@@ -20,6 +11,24 @@ const mapOptions = {
 }
 
 </script>
+
+<template>
+  <div class="map" >
+    <Header></Header>
+  </div>
+  <div class="map" id="app" >
+    <HealthMap />
+  </div>
+  <div>
+    <naver-map style="width: 100vw; height: 100vh;" :map-options="mapOptions">
+        <naver-marker latitude="37.51347" longitude="127.041722" />
+    </naver-map>
+  </div>
+  <div>
+    <Footer></Footer>
+  </div>
+</template>
+
 
 <style lang="scss" scoped>
 #app {
